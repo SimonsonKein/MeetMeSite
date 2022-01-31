4a6d67462e72d20dacc1e5573b35d432e5a05ca8
SimonsonKein <55325614+SimonsonKein@users.noreply.github.com>
2022-01-31 18:32:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+// СЛАЙДЕР
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider .slider-line');
+let count = 0;
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+
+}
+
+// АНИМАЦИЯ
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
+
+
+// СКОЛЛ В КОНЕЦ
+function scrollTo(element) {
+  window.scroll({
+    left: 0, 
+    top: element.offsetTop, 
+    behavior: 'smooth',
+  })
+}
+
+var sec1_button = document.querySelector('#sec1-button');
+var about_us = document.querySelector('#sec7');
+
+sec1_button.addEventListener('click', () => {
+  scrollTo(about_us);
+})
+
+function scrollTo(element) {
+  window.scroll({
+    left: 0, 
+    top: element.offsetTop, 
+    behavior: 'smooth',
+  })
+}
+
+
+
+var sec1_button = document.querySelector('#sec2-button');
+var about_us = document.querySelector('#sec7');
+
+sec1_button.addEventListener('click', () => {
+  scrollTo(about_us);
+})
+
+
+// ПЛАВНЫЙ  СКРОЛЛ
+$(document).bind( 'mousewheel', function (e) { 
+  var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+  e.preventDefault(); 
+  e.stopPropagation(); 
+  $(document.body).stop().animate( { 
+       scrollTop : nt 
+   } , 500 , 'easeInOutCubic' );  
+} )
+
+// ПРЕЛОУДЕР
+$(window).on('load', function () {
+  $preloader = $('.loaderArea'),
+    $loader = $preloader.find('.loader');
+  $loader.fadeOut();
+  $preloader.delay(650).fadeOut('slow');
+});
